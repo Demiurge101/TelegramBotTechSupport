978dc6e2e22788d564dee838afaddb4a29828b27
GFMws <ydalite01@gmail.com>
2023-09-19 13:18:59 +0400

    insert

---
+++ dbscripts/ts_fill.sql
@@ -0,0 +1,55 @@
+USE TS_Dispatcher;
+
+
+insert into titles (parent_id, title, title_type) values (0, "Проблемы с оборудованием КЕДР", 1);
+insert into titles (parent_id, title, title_type) values (0, "Проблемы с сетью", 1);
+insert into titles (parent_id, title, title_type) values (0, "Проблемы с программами DCSoft", 1);
+insert into titles (parent_id, title, title_type) values (0, "Система одного номера", 1);
+
+insert into titles (parent_id, title, title_type) values (1, "УСО", 1);
+insert into titles (parent_id, title, title_type) values (1, "Пульт бурильщика", 1);
+insert into titles (parent_id, title, title_type) values (1, "Датчики", 1);
+insert into titles (parent_id, title, title_type) values (1, "Кабели", 1);
+
+
+insert into titles (parent_id, title, title_type) values (2, "WiFi точки", 1);
+insert into titles (parent_id, title, title_type) values (2, "Камеры", 1);
+insert into titles (parent_id, title, title_type) values (2, "IP адреса", 1);
+insert into titles (parent_id, title, title_type) values (2, "IP телефоны и АТС", 1);
+
+
+insert into titles (parent_id, title, title_type) values (3, "DCServer", 1);
+insert into titles (parent_id, title, title_type) values (3, "DCPlot", 1);
+insert into titles (parent_id, title, title_type) values (3, "DCDevice", 1);
+
+
+insert into titles (parent_id, title, title_type) values (5, "УСО Exd PowerLine", 2);
+insert into titles (parent_id, title, title_type) values (5, "УСО Exd WDSL", 2);
+insert into titles (parent_id, title, title_type) values (5, "УСО Exn WDSL", 2);
+
+insert into titles (parent_id, title, title_type) values (6, "ПНД Exd PowerLine", 2);
+insert into titles (parent_id, title, title_type) values (6, "ПНД Exd WDSL", 2);
+insert into titles (parent_id, title, title_type) values (6, "ПНД Exn WDSL", 2);
+
+insert into titles (parent_id, title, title_type) values (7, "ДНК", 2);
+insert into titles (parent_id, title, title_type) values (7, "ДДИ", 2);
+insert into titles (parent_id, title, title_type) values (7, "ДУП", 2);
+insert into titles (parent_id, title, title_type) values (7, "РУД", 2);
+insert into titles (parent_id, title, title_type) values (7, "ДОП-М", 2);
+insert into titles (parent_id, title, title_type) values (7, "БЗУД", 2);
+insert into titles (parent_id, title, title_type) values (7, "ДТ", 2);
+
+
+insert into titles (parent_id, title, title_type) values (8, "Кабели датчиков ГТИ", 2);
+insert into titles (parent_id, title, title_type) values (8, "Магистральные кабели", 2);
+
+
+insert into titles (parent_id, title, title_type) values (9, "Ubiquiti", 2);
+insert into titles (parent_id, title, title_type) values (9, "TP-Link", 2);
+
+
+insert into titles (parent_id, title, title_type) values (12, "Fanvil X1", 2);
+insert into titles (parent_id, title, title_type) values (12, "Fanvil X4", 2);
+insert into titles (parent_id, title, title_type) values (12, "Yeastar S20", 2);
+insert into titles (parent_id, title, title_type) values (12, "Yeastar S50", 2);
+
