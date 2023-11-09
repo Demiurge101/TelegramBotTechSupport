d8b7c3a6be67c0901db4039fdb6c7b99eacc1af4
GFMws <ydalite01@gmail.com>
2023-11-09 16:49:54 +0400

    fix db_...

--- build_DB.py
+++ build_DB.py
@@ -10,7 +10,7 @@ clients = Config.clients
 source_location = os.path.abspath(source_location)
 source_list = os.listdir(source_location)
 
-SN = MDataBase.SonDB(db_host, db_login, Config.password, Config.bd_name_dispatcher_son)
+SN = MDataBase.SonDB(Config.db_host, Config.db_login, Config.password, Config.bd_name_dispatcher_son)
 SN.connect()
 
 for client in Config.clients:
