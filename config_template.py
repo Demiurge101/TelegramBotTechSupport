02cfe3b5ad52c1b7454c01fcb573964d7d28f7bd
GFMws <ydalite01@gmail.com>
2023-11-13 14:06:32 +0400

    config template

---
+++ config_template.py
@@ -0,0 +1,25 @@
+
+
+db_host = ''
+db_port = 0
+db_login = ''
+db_password = ''
+
+
+db_name_ts = ""
+db_name_son = ""
+db_name_dispatcher_son = ""
+db_name_dispatcher_ts = ""
+
+Token = ""
+MyToken = ""
+
+sonDBfiles = ""
+# tsDBfiles = "//falcon/marinin/TS"
+tsDBfiles = "C:/Share/TS"
+mkcb_location = ""
+
+clients = {}
+
+
+admins = {}
