b8117b85a4ceaee7243bb05a0057dab2e680181d
gfm_ws <workstation@gfm.com>
2024-03-01 15:42:47 +0900

    logo

--- webinterface/webinterface/settings.py
+++ webinterface/webinterface/settings.py
@@ -132,10 +132,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = BASE_DIR / 'static'
 
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
+    # BASE_DIR / "static" ,
+    STATIC_ROOT / 'img',
 ]
 # STATICFILES_DIR=[
 # os.path.join(PROJECT_ROOT,"static"),
