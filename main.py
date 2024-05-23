d028e1c57da821fe2d429c5358910425153ef18c
GFMws <ydalite01@gmail.com>
2024-05-23 13:36:07 +0400

    research

--- main.py
+++ main.py
@@ -712,10 +712,11 @@ def son(message, menu_id=0, overcount=0):
             print(f"get: {files_obj}")
         # print(f"get: {files_obj}")
         for file_obj in files_obj:
-            print(f"{file_obj}: {son_controller.getTextByCode(file_obj.lower())}") #debug
+            cd = file_obj.lower()
+            print(f"{file_obj}: {son_controller.getTextByCode(cd)}") #debug
             for ch in file_obj: #debug
                 print(f"{ch} = {ord(ch)}") #debug
-            sub_menu.append(son_controller.getTextByCode(file_obj.lower()))
+            sub_menu.append(son_controller.getTextByCode(cd))
 
         # print("sub_menu:", sub_menu)
         res = '-'
